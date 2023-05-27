f9cd11d79498dbb112096be3477b76a6d25aaa9d
Xuradel <leagueofxuradel@gmail.com>
2023-05-26 23:22:41 -0600

    animation changes

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,25 +1,38 @@
+import { Fade } from "react-awesome-reveal";
+
 const Achievments = () => {
     return (
         <div class="card">
             <div class="item item">
-                <img className="logos" src={require("../images/champion.png")}></img>
-                <span class="quantity"> 2 </span>
-                <span class="text text"> Champions </span>
+                <Fade delay={100}><img className="logos" src={require("../images/champion.png")}></img></Fade>
+                <Fade delay={200} className="test1"><span class="quantity"> 2 </span></Fade>
+                <Fade delay={300} className="test1"><span class="text text"> Champions </span></Fade>
+
+
             </div>
             <div class="item item">
-                <img className="logos" src={require("../images/2champion.png")}></img>
-                <span class="quantity"> 1 </span>
-                <span class="text text"> Sub-Champion </span>
+                <Fade delay={400}><img className="logos" src={require("../images/2champion.png")}></img></Fade>
+                <Fade delay={500} className="test1"><span class="quantity"> 1 </span></Fade>
+                <Fade delay={600} className="test1"><span class="text text"> Sub-Champion </span></Fade>
+
+
+
             </div>
             <div class="item item">
-                <img className="logos" src={require("../images/staff.png")}></img>
-                <span class="quantity"> 8+ </span>
-                <span class="text text"> Staff LLA & LVP </span>
+                <Fade delay={700}><img className="logos" src={require("../images/staff.png")}></img></Fade>
+                <Fade delay={800} className="test1"><span class="quantity" color="white"> 8+ </span></Fade>
+                <Fade delay={900} className="test1"><span class="text text"> Staff LLA & LVP </span></Fade>
+
+
+
             </div>
             <div class="item item">
-                <img className="logos" src={require("../images/tracking.png")}></img>
-                <span class="quantity"> 100% </span>
-                <span class="text text"> Accurate tracking </span>
+                <Fade delay={1000}><img className="logos" src={require("../images/tracking.png")}></img></Fade>
+                <Fade delay={1100} className="test1"><span class="quantity"> 100% </span></Fade>
+                <Fade delay={1200} className="test1"><span class="text text"> Accurate tracking </span></Fade>
+
+
+
             </div>
         </div>
     )
--- src/components/QuickFeatures.js
+++ src/components/QuickFeatures.js
@@ -11,59 +11,35 @@ const QuickFeatures = () => {
 
     return (
         <div className="features-1" id="features-section">
-            {!isMobile ? <div className="features1-item">
-                <div className="features1-inside" data-aos="fade-left">
+            <div className="features1-item">
+                <div className="features1-inside" data-aos="fade-in">
                     <img src={require("../images/depth.png")} className="features1-icon"></img>
                     <h2>IN DEPTH <span className="gold">TRACKING</span></h2>
                 </div>
-                <p data-aos="fade-up">Track every second of data, starting from lobby, champion selection, in-game
+                <p data-aos="fade-out">Track every second of data, starting from lobby, champion selection, in-game
                     and finally record post-game statistics.
                 </p>
-            </div> : <div className="features1-item">
-                <div className="features1-inside" >
-                    <img src={require("../images/depth.png")} className="features1-icon"></img>
-                    <h2>IN DEPTH <span className="gold">TRACKING</span></h2>
-                </div>
-                <p >Track every second of data, starting from lobby, champion selection, in-game
-                    and finally record post-game statistics.
-                </p>
-            </div>}
+            </div>
 
-            {!isMobile ? <div className="features1-item">
-                <div className="features1-inside" data-aos="fade-left">
+            <div className="features1-item">
+                <div className="features1-inside" data-aos="fade-in">
                     <img src={require("../images/analyze.png")} className="features1-icon"></img>
                     <h2>ACCURATE <span className="gold">INFORMATION</span></h2>
                 </div>
-                <p data-aos="fade-up">Analyze every move and data recorded by our application every second.
+                <p data-aos="fade-out">Analyze every move and data recorded by our application every second.
                     You will have access to precise information.
                 </p>
-            </div> : <div className="features1-item">
-                <div className="features1-inside" >
-                    <img src={require("../images/analyze.png")} className="features1-icon"></img>
-                    <h2>ACCURATE <span className="gold">INFORMATION</span></h2>
-                </div>
-                <p >Analyze every move and data recorded by our application every second.
-                    You will have access to precise information.
-                </p>
-            </div>}
+            </div>
 
-            {!isMobile ? <div className="features1-item">
-                <div className="features1-inside" data-aos="fade-left">
+            <div className="features1-item">
+                <div className="features1-inside" data-aos="fade-in">
                     <img src={require("../images/data.png")} className="features1-icon"></img>
                     <h2>RELIABLE <span className="gold">DATA</span></h2>
                 </div>
-                <p data-aos="fade-up">Our data is reliable since it uses League's API and our code process
+                <p data-aos="fade-out">Our data is reliable since it uses League's API and our code process
                     the information in a way it's recorded without mistakes.
                 </p>
-            </div> : <div className="features1-item">
-                <div className="features1-inside" >
-                    <img src={require("../images/data.png")} className="features1-icon"></img>
-                    <h2>RELIABLE <span className="gold">DATA</span></h2>
-                </div>
-                <p >Our data is reliable since it uses League's API and our code process
-                    the information in a way it's recorded without mistakes.
-                </p>
-            </div>}
+            </div>
 
         </div>
     )
